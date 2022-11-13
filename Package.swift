a9581472058c1834d0e870ef2ada640fe441559c
glushchenko <fluder@icloud.com>
2022-11-13 20:50:29 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "SoulverCore",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+    ],
     products: [
         .library(
             name: "SoulverCore",
